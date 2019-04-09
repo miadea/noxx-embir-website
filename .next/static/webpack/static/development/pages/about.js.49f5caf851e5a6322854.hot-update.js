b1319d60c4e7d588cc3eb1bf50940c55fe734162
Sivli Kestanous <cmtoups@me.com>
2019-04-09 14:21:21 -0400

    init

---
+++ .next/static/webpack/static/development/pages/about.js.49f5caf851e5a6322854.hot-update.js
@@ -0,0 +1,168 @@
+webpackHotUpdate("static/development/pages/about.js",{
+
+/***/ "./pages/about.js":
+/*!************************!*\
+  !*** ./pages/about.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return About; });
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_objectSpread__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/objectSpread */ "./node_modules/@babel/runtime-corejs2/helpers/esm/objectSpread.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_classCallCheck__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/classCallCheck */ "./node_modules/@babel/runtime-corejs2/helpers/esm/classCallCheck.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_createClass__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/createClass */ "./node_modules/@babel/runtime-corejs2/helpers/esm/createClass.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_possibleConstructorReturn__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/possibleConstructorReturn */ "./node_modules/@babel/runtime-corejs2/helpers/esm/possibleConstructorReturn.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_getPrototypeOf__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/getPrototypeOf */ "./node_modules/@babel/runtime-corejs2/helpers/esm/getPrototypeOf.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_assertThisInitialized__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/assertThisInitialized */ "./node_modules/@babel/runtime-corejs2/helpers/esm/assertThisInitialized.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_inherits__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/inherits */ "./node_modules/@babel/runtime-corejs2/helpers/esm/inherits.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_defineProperty__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/defineProperty */ "./node_modules/@babel/runtime-corejs2/helpers/esm/defineProperty.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_8___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_8__);
+/* harmony import */ var _components_MyLayout_js__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ../components/MyLayout.js */ "./components/MyLayout.js");
+/* harmony import */ var react_photo_gallery__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! react-photo-gallery */ "./node_modules/react-photo-gallery/dist/react-photo-gallery.esm.js");
+/* harmony import */ var react_images__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! react-images */ "./node_modules/react-images/lib/Lightbox.js");
+/* harmony import */ var react_images__WEBPACK_IMPORTED_MODULE_11___default = /*#__PURE__*/__webpack_require__.n(react_images__WEBPACK_IMPORTED_MODULE_11__);
+
+
+
+
+
+
+
+
+var _jsxFileName = "/Users/kestanous/dev/play/noxx-embir-website/pages/about.js";
+
+
+
+
+var IMAGES = [{
+  src: "https://noxxembirphotostorage.sfo2.digitaloceanspaces.com/Product/180909_EMS_20038.jpg",
+  thumbnail: "https://noxxembirphotostorage.sfo2.digitaloceanspaces.com/Product/180909_EMS_20038.jpg",
+  width: 4,
+  height: 1
+}, {
+  src: "https://noxxembirphotostorage.sfo2.digitaloceanspaces.com/Product/180924_EMS0068-2.jpg",
+  thumbnail: "https://noxxembirphotostorage.sfo2.digitaloceanspaces.com/Product/180924_EMS0068-2.jpg",
+  width: 1,
+  height: 2
+}, {
+  src: "https://noxxembirphotostorage.sfo2.digitaloceanspaces.com/Product/181001_EMS0029.jpg",
+  thumbnail: "https://noxxembirphotostorage.sfo2.digitaloceanspaces.com/Product/181001_EMS0029.jpg",
+  width: 4,
+  height: 1
+}, {
+  src: "https://noxxembirphotostorage.sfo2.digitaloceanspaces.com/Product/181105_EMS0056%20copy.jpg",
+  thumbnail: "https://noxxembirphotostorage.sfo2.digitaloceanspaces.com/Product/181105_EMS0056%20copy.jpg",
+  width: 4,
+  height: 1
+}, {
+  src: "https://noxxembirphotostorage.sfo2.digitaloceanspaces.com/Product/181201_EMS02177_3.jpg",
+  thumbnail: "https://noxxembirphotostorage.sfo2.digitaloceanspaces.com/Product/181201_EMS02177_3.jpg",
+  width: 4,
+  height: 1
+}, {
+  src: "https://noxxembirphotostorage.sfo2.digitaloceanspaces.com/Product/181206_EMS0056.jpg",
+  thumbnail: "https://noxxembirphotostorage.sfo2.digitaloceanspaces.com/Product/181206_EMS0056.jpg",
+  width: 4,
+  height: 1
+}];
+
+var About =
+/*#__PURE__*/
+function (_React$Component) {
+  Object(_babel_runtime_corejs2_helpers_esm_inherits__WEBPACK_IMPORTED_MODULE_6__["default"])(About, _React$Component);
+
+  function About() {
+    var _getPrototypeOf2;
+
+    var _this;
+
+    Object(_babel_runtime_corejs2_helpers_esm_classCallCheck__WEBPACK_IMPORTED_MODULE_1__["default"])(this, About);
+
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    _this = Object(_babel_runtime_corejs2_helpers_esm_possibleConstructorReturn__WEBPACK_IMPORTED_MODULE_3__["default"])(this, (_getPrototypeOf2 = Object(_babel_runtime_corejs2_helpers_esm_getPrototypeOf__WEBPACK_IMPORTED_MODULE_4__["default"])(About)).call.apply(_getPrototypeOf2, [this].concat(args)));
+
+    Object(_babel_runtime_corejs2_helpers_esm_defineProperty__WEBPACK_IMPORTED_MODULE_7__["default"])(Object(_babel_runtime_corejs2_helpers_esm_assertThisInitialized__WEBPACK_IMPORTED_MODULE_5__["default"])(_this), "state", {
+      currentImage: 0
+    });
+
+    Object(_babel_runtime_corejs2_helpers_esm_defineProperty__WEBPACK_IMPORTED_MODULE_7__["default"])(Object(_babel_runtime_corejs2_helpers_esm_assertThisInitialized__WEBPACK_IMPORTED_MODULE_5__["default"])(_this), "openLightbox", function (event, obj) {
+      _this.setState({
+        currentImage: obj.index,
+        lightboxIsOpen: true
+      });
+    });
+
+    Object(_babel_runtime_corejs2_helpers_esm_defineProperty__WEBPACK_IMPORTED_MODULE_7__["default"])(Object(_babel_runtime_corejs2_helpers_esm_assertThisInitialized__WEBPACK_IMPORTED_MODULE_5__["default"])(_this), "closeLightbox", function () {
+      _this.setState({
+        currentImage: 0,
+        lightboxIsOpen: false
+      });
+    });
+
+    Object(_babel_runtime_corejs2_helpers_esm_defineProperty__WEBPACK_IMPORTED_MODULE_7__["default"])(Object(_babel_runtime_corejs2_helpers_esm_assertThisInitialized__WEBPACK_IMPORTED_MODULE_5__["default"])(_this), "gotoPrevious", function () {
+      _this.setState({
+        currentImage: _this.state.currentImage - 1
+      });
+    });
+
+    Object(_babel_runtime_corejs2_helpers_esm_defineProperty__WEBPACK_IMPORTED_MODULE_7__["default"])(Object(_babel_runtime_corejs2_helpers_esm_assertThisInitialized__WEBPACK_IMPORTED_MODULE_5__["default"])(_this), "gotoNext", function () {
+      _this.setState({
+        currentImage: _this.state.currentImage + 1
+      });
+    });
+
+    return _this;
+  }
+
+  Object(_babel_runtime_corejs2_helpers_esm_createClass__WEBPACK_IMPORTED_MODULE_2__["default"])(About, [{
+    key: "render",
+    value: function render() {
+      return react__WEBPACK_IMPORTED_MODULE_8___default.a.createElement(_components_MyLayout_js__WEBPACK_IMPORTED_MODULE_9__["default"], {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 49
+        },
+        __self: this
+      }, react__WEBPACK_IMPORTED_MODULE_8___default.a.createElement(react_photo_gallery__WEBPACK_IMPORTED_MODULE_10__["default"], {
+        onClick: this.openLightbox,
+        photos: IMAGES.map(function (image) {
+          return Object(_babel_runtime_corejs2_helpers_esm_objectSpread__WEBPACK_IMPORTED_MODULE_0__["default"])({}, image, {
+            src: image.thumbnail
+          });
+        }),
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 50
+        },
+        __self: this
+      }), react__WEBPACK_IMPORTED_MODULE_8___default.a.createElement(react_images__WEBPACK_IMPORTED_MODULE_11___default.a, {
+        images: IMAGES,
+        onClose: this.closeLightbox,
+        onClickPrev: this.gotoPrevious,
+        onClickNext: this.gotoNext,
+        currentImage: this.state.currentImage,
+        isOpen: this.state.lightboxIsOpen,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 52
+        },
+        __self: this
+      }));
+    }
+  }]);
+
+  return About;
+}(react__WEBPACK_IMPORTED_MODULE_8___default.a.Component);
+
+
+
+/***/ })
+
+})
+//# sourceMappingURL=about.js.49f5caf851e5a6322854.hot-update.js.map
